25daef076070cc62d71251618d42779c312c8dc6
HannanDeveloper1 <hannandeveloper1@gmail.com>
2024-12-29 14:49:43 +0500

    Handle Route not found error

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' }); 
+});
+
 export default app;
 
 app.use(Error)
